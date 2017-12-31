ebb7f7c579f31f7d159ae17909898eaf0ae851ca
Joshua Niederer <josh@jpniederer.com>
2017-12-30 21:59:37 -0600

    Playing with some basic language features.

---
+++ LanguageFeatures/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playing with basic language features.
+
+import UIKit
+
+// Variables
+var str = "Hello, playground"
+var i = 10
+// Declare a constant.
+let daysInWeek = 7
+var d: Double
+d = 10.0
+var anInt: Int
+anInt = Int(d)
+
+func square(x: Int) -> Int {
+    return x * x
+}
+
+square(x: i)
+
