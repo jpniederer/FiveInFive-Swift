348d57cd8f11f8c4e943f26312aa96e111b81e55
Joshua Niederer <josh@jpniederer.com>
2018-01-01 20:27:08 -0600

    Writing a basic test in an iOS project with Swift.

---
+++ TestingSwift/TestingWithSwift/TestingWithSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingWithSwift
+//
+//  Created by Joshua Niederer on 1/1/18.
+//  Copyright © 2018 Joshua Niederer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
